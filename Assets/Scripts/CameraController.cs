5b700bd067cc3f4f6d3d69a39dcee3cb5201a856
Pia <pia.s.schmidt@googlemail.com>
2019-05-24 17:34:22 +0200

    Bug "fixed". not my part

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,10 +10,10 @@ public class CameraController : MonoBehaviour
     public float sensitivity = 10f;
     void Update()
     {
-        var c = Camera.main.transform;
-        c.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
-        c.Rotate(-Input.GetAxis("Mouse Y") * sensitivity, 0, 0);
-        c.Rotate(0, 0, -Input.GetAxis("QandE") * 90 * Time.deltaTime);
+        //var c = Camera.main.transform;
+        //c.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        //c.Rotate(-Input.GetAxis("Mouse Y") * sensitivity, 0, 0);
+        //c.Rotate(0, 0, -Input.GetAxis("QandE") * 90 * Time.deltaTime);
         //if (Input.GetMouseButtonDown(0))
             //Cursor.lockState = CursorLockMode.Locked;
     }
