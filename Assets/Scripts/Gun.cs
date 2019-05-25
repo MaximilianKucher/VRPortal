f69ee2942e533792a468e92ce329137cbb2325f1
Nico <Nico.Wellermann@live.de>
2019-05-25 20:16:21 +0200

    Movement and shooting added
    Added the ability to freely look and move around the map. With left click a shot is fired.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // The Camera object from the players FOV
+    public Camera fpsCam;
+
+    // Just to recognize a mouse pressed button
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot(true);
+        }
+        else if (Input.GetKeyDown("Fire3"))
+        {
+            Shoot(false);
+        }
+    }
+    /// <summary>
+    /// Raycast a shot. With the hit variable the position of the hit can be determind to spawn a portal. 
+    /// </summary>
+    /// <param name="bluePortal">To different between a left and the right click portal</param>
+    void Shoot(bool bluePortal)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,95 +10,23 @@ public class Boundary
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private string mouseXInputName, mouseYInputName;
-    [SerializeField] private float mouseSensitivity;
-
-    private float xAxisClamp;
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
 
     private Vector3 position;
-    private Vector3 rotation;
 
-    public float movementSpeed = 0.2f;
-    public float rotationSpeed = 0.2f;
-
+    /// <summary>
+    /// Smoothed the movement of the player with variable Input Methods.
+    /// </summary>
     void Update()
     {
-        position = this.transform.position;
-        if (Input.GetKey(KeyCode.D))
-        {
-            position.x += movementSpeed;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            position.x -= movementSpeed;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            position.z -= movementSpeed;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            position.z += movementSpeed;
-            this.transform.position = position;
-        }
-        /*
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
 
-        if (Input.GetMouseButtonDown(1))    //right mouse button
-        {
-             //CameraRotation();
-            rotation = new Vector3(Input.GetAxis("Mouse Y")* 10, Input.GetAxis("Mouse X")*10, 0f);
-          
-            print("Rotation: " + rotation);
-            if (Input.GetAxis("Mouse X") < 0)
-            {
-               // transform.Rotate(rotation * Time.deltaTime * rotationSpeed);
-                transform.Rotate(Vector3.up * rotationSpeed);
-            }
-            else
-            {
-                //transform.Rotate(rotation * Time.deltaTime * -rotationSpeed);
-                  transform.Rotate(Vector3.up * -rotationSpeed);
-            }
-                
-      
-        }*/
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        transform.position += (forwardMovement + rightMovement);
     }
-
-    //TODO
-    private void CameraRotation()
-    {
-        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
-        print("Y " + mouseY);
-        print("X " + mouseX);
-        xAxisClamp += mouseY;
-
-        if (xAxisClamp > 90.0f)
-        {
-            xAxisClamp = 90.0f;
-            mouseY = 0.0f;
-            ClampXAxisRotationToValue(270.0f);
-        }
-        else if (xAxisClamp < -90.0f)
-        {
-            xAxisClamp = -90.0f;
-            mouseY = 0.0f;
-            ClampXAxisRotationToValue(90.0f);
-        }
-        //print(mouseX);
-        //this.transform.Rotate(Vector3.left * mouseY);
-        //this.transform.Rotate(Vector3.up * mouseX);
-    }
-
-    private void ClampXAxisRotationToValue(float value)
-    {
-        Vector3 eulerRotation = transform.eulerAngles;
-        eulerRotation.x = value;
-        this.transform.eulerAngles = eulerRotation;
-    }
-
-
 }
